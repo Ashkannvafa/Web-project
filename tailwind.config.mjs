fd3d250dc5d6e1bf5d416c59e18569e073a1e215
Ashkan <165916280+Ashkannvafa@users.noreply.github.com>
2025-11-30 09:47:36 -0800

    Add files via upload

---
+++ tailwind.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/context/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    darkMode: "selector",
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
